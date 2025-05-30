17cbe621bef71630b56483cc672b1750c5eedbd9
MBVD <vasiliidiu3102@gmail.com>
2025-05-30 05:43:19 +0500

    add symbols

---
+++ inc/symbol.hpp
@@ -0,0 +1,54 @@
+#include <type.hpp>
+
+struct Symbol {
+    Symbol(const std::shared_ptr<Type>& type, bool is_const = false)
+        : type(type), is_const(is_const) {}
+    virtual ~Symbol() = default;
+
+    virtual bool is_var() {
+        return false;
+    }
+
+    virtual bool is_func() {
+        return false;
+    }
+
+    virtual bool is_record() {
+        return false;
+    }
+
+    std::shared_ptr<Type> type;
+    bool is_const;
+};
+
+struct VarSymbol : public Symbol {
+    VarSymbol(const std::shared_ptr<Type>& type, bool is_const = false)
+        : Symbol(type, is_const) {}
+
+    VarSymbol(const std::shared_ptr<Type>& type, const std::any& value, bool is_const = false)
+        : Symbol(type, is_const), value(value) {}
+    bool is_var() {
+        return true;
+    } 
+
+
+    std::any value;
+};
+
+struct FuncSymbol : public Symbol {
+    FuncSymbol(const std::shared_ptr<FuncType>& type, bool is_const = false)
+        : Symbol(type, is_const) {}
+    bool is_func() {
+        return true; 
+    }
+};
+
+struct Record : public Symbol {
+    Record(const std::shared_ptr<RecordType>& type, const std::unordered_map<std::string, std::shared_ptr<Symbol>>& ,bool is_const = false)
+        : Symbol(type, is_const), fields(fields){}
+    bool is_record() { 
+        return true; 
+    }
+
+    std::unordered_map<std::string, std::shared_ptr<Symbol>> fields;
+};
